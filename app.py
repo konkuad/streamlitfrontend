2b97c6bf407d2060f2a4805d3fb8bfe4658b0f5e
Punnawish Thuwajit <konkuad2@gmail.com>
2022-09-08 23:07:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with st.sidebar:
         random_alpha = False
         modulation_json = presets.get_presets()[option]
 
-url = 'http://4400-35-229-120-189.ngrok.io'
+url = 'http://df15-104-196-22-141.ngrok.io'
 
 if generate:
     
